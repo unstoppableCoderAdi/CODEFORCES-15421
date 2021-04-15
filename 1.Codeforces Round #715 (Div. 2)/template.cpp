240238ada683c3beaf161e0321cdda84af0fa1e4
unstoppableCoderAdi <80784437+unstoppableCoderAdi@users.noreply.github.com>
2021-04-15 14:01:54 +0530

    Add files via upload

---
+++ 1.Codeforces Round #715 (Div. 2)/template.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void fast() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+#ifndef ONLINE_JUDGE
+	//for getting input from input.txt
+	freopen("input.txt", "r", stdin);
+	//for getting output to output.txt
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+
+}
+
+int main() {
+	fast();
+
+	int tt;
+	cin >> tt;
+	while (tt--)
+	{
+		solve();
+		cout << "\n";
+	}
+
+
+	return 0;
+}
